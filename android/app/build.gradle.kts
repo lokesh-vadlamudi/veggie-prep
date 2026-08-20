0de32ef597098e5aa9bfc5cb5827fcc4403cb0fc
Lokesh Vadlamudi <lokv007@gmail.com>
2026-08-20 08:59:39 -0700

    Bump Android version code for Play upload

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.lokeshvadlamudi.veggieprep"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "0.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
